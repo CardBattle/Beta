f7b65f8b3d2b1d9938fe7cb5badef1c1544032ad
ProPim <qusdnals3@naver.com>
2023-08-01 11:26:56 +0900

    UIManager 코드 수정 및 추가

--- Card Battle/Assets/Script/UIManager.cs
+++ Card Battle/Assets/Script/UIManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
-    public static UIManager Um;  
+    public static UIManager Um;
     public Text result;
     [SerializeField]
     private GameObject battleUI;
@@ -14,11 +14,18 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private AudioSource bgm;
 
+    //public CardManager uicardManager;
+
     public Sprite[] playerDiceSprite;
     public Sprite[] enemyDiceSprite;
 
+    public List<GameObject> deckList;
+    //  public List<Card> DeckListData;
+
     public GameObject playerDice;
     public GameObject enemyDice;
+
+    public GameObject deckListUI;
 
     public GameObject canvasUI;
     public GameObject defeatUI;
@@ -26,6 +33,9 @@ public class UIManager : MonoBehaviour
 
     public Slider bgmVol;
     public Slider soundEffVol;
+
+    private int row = 7;
+    private float xDistance = 2.3f;
 
     public void Awake()
     {
@@ -40,7 +50,7 @@ public class UIManager : MonoBehaviour
     public void BgmVol()
     {
         bgm.volume = bgmVol.value;
-        PlayerPrefs.SetFloat("BGMVol",bgmVol.value);
+        PlayerPrefs.SetFloat("BGMVol", bgmVol.value);
         PlayerPrefs.Save();
     }
     public void ReStart()
@@ -49,6 +59,11 @@ public class UIManager : MonoBehaviour
     }
     public void Menu()
     {
+        if (BattleManager.Bm.state == BattleManager.State.WaitState)
+            BattleManager.Bm.state = BattleManager.State.Menu;
+        else if (BattleManager.Bm.state == BattleManager.State.SelectCard)
+            BattleManager.Bm.state = BattleManager.State.SelectMenu;
+
         Transform canvasUITransform = canvasUI.transform;
         Time.timeScale = 0;
         battleUI.SetActive(false);
@@ -58,11 +73,58 @@ public class UIManager : MonoBehaviour
         canvasUITransform.transform.GetChild(5).gameObject.SetActive(true);
         canvasUITransform.transform.GetChild(6).gameObject.SetActive(true);
         canvasUITransform.transform.GetChild(7).gameObject.SetActive(true);
+    }
+    public void DeckCardTransform()
+    {
+        for (int i = 0; i < deckList.Count; i++)
+        {
+            deckList[i].GetComponent<Collider2D>().enabled = false;
+            deckList[i].GetComponent<Card>().originPRS = DeckCardSort(i, row);
+            deckList[i].GetComponent<Card>().MoveTransform(deckList[i].GetComponent<Card>().originPRS, false);
+            deckList[i].GetComponent<Card>().CardFront(true);
+            deckList[i].GetComponent<Order>().DeckSettingOrder(0);
+            deckList[i].SetActive(false);
+        }
+    }
+    public void DeckList()
+    {
+        if (BattleManager.Bm.state == BattleManager.State.WaitState)
+            BattleManager.Bm.state = BattleManager.State.Menu;
+        else if (BattleManager.Bm.state == BattleManager.State.SelectCard)
+            BattleManager.Bm.state = BattleManager.State.SelectMenu;
 
 
+        Time.timeScale = 0;
+        battleUI.SetActive(false);
+        deckListUI.SetActive(true);
+
+        for (int i = 0; i < deckList.Count; i++)
+        {
+            deckList[i].SetActive(true);
+        }
+    }
+    public void DeckListReturn()
+    {
+        if (BattleManager.Bm.state == BattleManager.State.Menu)
+            BattleManager.Bm.state = BattleManager.State.WaitState;
+        else if (BattleManager.Bm.state == BattleManager.State.SelectMenu)
+            BattleManager.Bm.state = BattleManager.State.SelectCard;
+
+        Time.timeScale = 1;
+        deckListUI.SetActive(false);
+        battleUI.SetActive(true);
+        for (int i = 0; i < deckList.Count; i++)
+        {
+            deckList[i].SetActive(false);
+        }
     }
     public void Return()
     {
+        if (BattleManager.Bm.state == BattleManager.State.Menu)
+            BattleManager.Bm.state = BattleManager.State.WaitState;
+        else if (BattleManager.Bm.state == BattleManager.State.SelectMenu)
+            BattleManager.Bm.state = BattleManager.State.SelectCard;
+
         Transform canvasUITransform = canvasUI.transform;
         Time.timeScale = 1;
         menuUI.SetActive(false);
@@ -72,7 +134,7 @@ public class UIManager : MonoBehaviour
         canvasUITransform.transform.GetChild(6).gameObject.SetActive(false);
         canvasUITransform.transform.GetChild(7).gameObject.SetActive(false);
         battleUI.SetActive(true);
-        
+
     }
 
     public void NextStage()
@@ -105,7 +167,7 @@ public class UIManager : MonoBehaviour
         if (BattleManager.Bm.stage == 2)
         {
             battleUI.transform.GetChild(3).gameObject.SetActive(false);
-            victoryText.text = "모든 스테이지를 클리어 하신걸 축하합니다.";        
+            victoryText.text = "모든 스테이지를 클리어 하신걸 축하합니다.";
             canvasUITransform.GetChild(0).gameObject.SetActive(false);
             canvasUITransform.GetChild(1).gameObject.SetActive(false);
             victoryUITransform.GetChild(4).gameObject.SetActive(false);
@@ -127,19 +189,19 @@ public class UIManager : MonoBehaviour
         battleUI.transform.GetChild(3).gameObject.SetActive(false);
         menuUI.SetActive(true);
         defeatUI.transform.GetChild(1).
-            gameObject.GetComponent<Text>().text = $"지나간 턴 수: {BattleManager.Bm.turnCount}";  
+            gameObject.GetComponent<Text>().text = $"지나간 턴 수: {BattleManager.Bm.turnCount}";
         defeatUI.SetActive(true);
     }
 
     public void ClearRank(int count)
     {
-      
-        if (count <= 5)     
+
+        if (count <= 5)
         {
             victoryUI.transform.GetChild(1).
-            gameObject.GetComponent<Text>().text = "S";     
-        }    
-        else if (count <= 10)    
+            gameObject.GetComponent<Text>().text = "S";
+        }
+        else if (count <= 10)
         {
             victoryUI.transform.GetChild(1).
             gameObject.GetComponent<Text>().text = "A";
@@ -166,10 +228,30 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    public IEnumerator ShufflingDice()
+    {
+        playerDice.SetActive(true);
+        enemyDice.SetActive(true);
+
+        SpriteRenderer playerDiceSprite = playerDice.GetComponent<SpriteRenderer>();
+        SpriteRenderer enemyDiceSprite = enemyDice.GetComponent<SpriteRenderer>();
+
+        for (int i = 0; i < 25; i++)
+        {
+            playerDiceSprite.sprite = this.playerDiceSprite[Random.Range(0, 6)];
+            enemyDiceSprite.sprite = this.enemyDiceSprite[Random.Range(0, 6)];
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        BattleManager.Bm.DiceResult();
+    }
+
     public IEnumerator Dice(int dice, bool player)
     {
+
         if (player)
         {
+            print($"플레이어 주사위 {dice}");
             SpriteRenderer diceSprite = playerDice.GetComponent<SpriteRenderer>();
             switch (dice)
             {
@@ -197,6 +279,7 @@ public class UIManager : MonoBehaviour
         }
         else if (!player)
         {
+            print($"적 주사위 {dice}");
             SpriteRenderer diceSprite = enemyDice.GetComponent<SpriteRenderer>();
             switch (dice)
             {
@@ -221,14 +304,55 @@ public class UIManager : MonoBehaviour
                 default:
                     break;
             }
-        }
 
-        playerDice.SetActive(true);
-        enemyDice.SetActive(true);
-       
-        yield return new WaitForSeconds(1f);
-       
-        playerDice.SetActive(false);
-        enemyDice.SetActive(false);
+            yield return new WaitForSeconds(1.5f);
+
+            BattleManager.Bm.BattleTurn();
+
+            playerDice.SetActive(false);
+            enemyDice.SetActive(false);
+        }
+    }
+
+    PRS DeckCardSort(int cardIndex, int row)
+    {
+        PRS result;
+        if (cardIndex < row)
+        {
+            if (cardIndex == 0)
+            {
+                result = new PRS(new Vector2(-6.9f, 3.2f), Utlis.Qi, Vector3.one);
+            }
+            else
+            {
+                result = new PRS(new Vector2(deckList[cardIndex - 1]
+                    .transform.position.x + xDistance, 3.2f), Utlis.Qi, Vector3.one);
+            }
+        }
+        else if (cardIndex < (row * 2))
+        {
+            if (cardIndex == 7)
+            {
+                result = new PRS(new Vector2(-6.9f, 0.5f), Utlis.Qi, Vector3.one);
+            }
+            else
+            {
+                result = new PRS(new Vector2(deckList[cardIndex - 1]
+                    .transform.position.x + xDistance, 0.5f), Utlis.Qi, Vector3.one);
+            }
+        }
+        else
+        {
+            if (cardIndex == 14)
+            {
+                result = new PRS(new Vector2(-6.9f, -2.2f), Utlis.Qi, Vector3.one);
+            }
+            else
+            {
+                result = new PRS(new Vector2(deckList[cardIndex - 1]
+                    .transform.position.x + xDistance, -2.2f), Utlis.Qi, Vector3.one);
+            }
+        }
+        return result;
     }
 }
