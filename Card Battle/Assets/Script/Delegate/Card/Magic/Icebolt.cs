0ba6c643dc8b771c2ba77a2d063529830d2b8b20
RudinP <song_e@sookmyung.ac.kr>
2023-08-03 14:58:50 +0900

    [feat]미구현된 카드 구현(#53)
    - Arrowshot +
    - Snapshot +
    - Fireball +
    - Ice bolt +
    구현
    
    Close #53

--- Card Battle/Assets/Script/Delegate/Card/Magic/Icebolt.cs
+++ Card Battle/Assets/Script/Delegate/Card/Magic/Icebolt.cs
@@ -6,10 +6,46 @@ public class Icebolt : CardUse
 {
     public override void Use(Character sender, Character receiver)
     {
-        base.Use(sender, receiver);
+        BattleManager bm = BattleManager.Bm;
+        if (sender.tag == "Player")
+        {
+            senderCard = bm.playerDecision.card;
+            receiverCard = bm.enemyDecision.card;
+        }
+        else
+        {
+            senderCard = bm.enemyDecision.card;
+            receiverCard = bm.playerDecision.card;
+        }
+
+        sender.GetComponent<SFXVFX>().play += PlaySFX;
+        sender.GetComponent<SFXVFX>().play += delegate () { Instantiate(vfx, sender.transform.position, Quaternion.identity); };
+
 
         receiver.info.Hp -= CalculateDmg(sender.info.AttackDmg, card.info.RandomDice, card.info.EffVal,
         CalculateEffect(card.info.Type, receiver.info.Weapon));
+
+        if (card.info.Level >= 2 || senderCard.info.Dice > receiverCard.info.Dice)
+        {
+            if (card.info.buffs.Count > 0)
+            {
+                bool isExist = false;
+                foreach (var buff in card.info.buffs)
+                {
+                    foreach (var exist in receiver.info.buffs)
+                    {
+                        if (exist.info.Id == buff.info.Id)
+                        {
+                            isExist = true;
+                            exist.info.CurrentTurn += buff.info.Turns;
+                            break;
+                        }
+                    }
+                    if (!isExist)
+                        receiver.info.buffs.Add(buff);
+                }
+            }
+        }
 
     }
 
